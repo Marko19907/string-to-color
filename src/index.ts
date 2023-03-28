1ec1f81fcc623bd5dd562b260fd907e456e00e1e
Marko19907 <marko.stancic23@gmail.com>
2023-03-28 16:01:12 +0200

    Added the generateColor and generateSecondaryColor functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-export const add = (a: number, b: number) => {
-  return a + b
+import seedrandom from "seedrandom";
+
+const PHI = 1.618033988749895;
+
+/**
+ * Generates a random color from the given string, saturation and lightness can be controlled.
+ * Inspired by https://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+ */
+export function generateColor(input: string, saturation = 85, lightness = 50): string {
+  return `hsl(
+    ${Math.floor(((seedrandom.xor128(input)() + 1 / PHI) % 1) * 360)}
+    , ${saturation}%, ${lightness}%
+  )`;
+}
+
+/**
+ * Same as generateColor() but with a different algorithm that produces a different color.
+ */
+export function generateSecondaryColor(
+    input: string,
+    saturation = 85,
+    lightness = 25
+): string {
+  return `hsl(
+    ${Math.floor(((seedrandom.xorwow(input)() + 1 / PHI) % 1) * 360)}
+    , ${saturation}%, ${lightness}%
+  )`;
 }
